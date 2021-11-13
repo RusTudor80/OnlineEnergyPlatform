22f8e3955b6153c975225bd3a32afe3b46c1eb5a
Tudor80 <rustudor70.com>
2021-11-13 02:09:02 +0200

    End of section 15

--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,13 +1,13 @@
 import { observer } from 'mobx-react-lite';
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { Grid } from 'semantic-ui-react';
 import LoadingComponent from '../../../app/layout/LoadingComponents';
 import { useStore } from '../../../app/stores/store';
 import ActivityDetailedChat from './ActivityDetailedChat';
-import ActivityDetailedHeader from './ActivityDetailedHeader';
 import ActivityDetailedInfo from './ActivityDetailedInfo';
 import ActivityDetailedSideBar from './ActivityDetailedSideBar';
+import ActivityDetailedHeader from './ActivityDetailedHeader';
 
 export default observer(function ActivityDetails() {
     const {activityStore} = useStore();
@@ -21,16 +21,15 @@ export default observer(function ActivityDetails() {
     if (loadingInitial || !activity) return <LoadingComponent />;
 
     return (
-       <Grid>
-           <Grid.Column width={10}>
-               <ActivityDetailedHeader activity={activity}/>
-               <ActivityDetailedInfo activity={activity}/>
-               <ActivityDetailedChat/>
-
-           </Grid.Column>
-           <Grid.Column width ={6}>
-               <ActivityDetailedSideBar/>
-           </Grid.Column>
-       </Grid>
+        <Grid>
+            <Grid.Column width={10}>
+                <ActivityDetailedHeader activity={activity} />
+                <ActivityDetailedInfo activity={activity} />
+                <ActivityDetailedChat />
+            </Grid.Column>
+            <Grid.Column width={6}>
+                <ActivityDetailedSideBar activity={activity} />
+            </Grid.Column>
+        </Grid>
     )
 })
